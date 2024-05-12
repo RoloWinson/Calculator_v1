e7b7e6d6fba213176a1c362c64a2968f6ff5b092
Rolo <proy4r@mail.ru>
2024-05-12 18:51:31 +0300

    add functionality
    Adding basic functionality

---
+++ script.js
@@ -0,0 +1,41 @@
+function appendToDisplay(value) {
+  document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+  document.getElementById('display').value = '';
+}
+
+function calculate() {
+  try {
+      var result = eval(document.getElementById('display').value);
+      document.getElementById('display').value = result;
+  } catch (error) {
+      document.getElementById('display').value = 'Ошибка';
+  }
+}
+
+// Флаг, указывающий на необходимость очистки поля ввода перед вводом нового числа или оператора
+let clearInput = false;
+
+function appendToDisplay(value) {
+    if (clearInput) {
+        clearDisplay();
+        clearInput = false;
+    }
+    document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function calculate() {
+    try {
+        var result = eval(document.getElementById('display').value);
+        document.getElementById('display').value = result;
+    } catch (error) {
+        document.getElementById('display').value = 'Ошибка';
+        clearInput = true;
+    }
+}
